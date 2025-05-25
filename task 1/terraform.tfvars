4119f877da39e31494ce46b7b59888e814b3e5f5
Ashmit Tyagi <151730397+Ashmit-Tyagi@users.noreply.github.com>
2025-05-25 20:42:28 +0530

    Add files via upload

---
+++ task 1/terraform.tfvars
@@ -0,0 +1,5 @@
+aws_region      = "ap-south-1"
+ami_id          = "ami-0e35ddab05955cf57"  
+instance_type   = "t2.micro"
+subnet_id       = "subnet-08ca56595bd5781ac"  
+ebs_volume_size = 12
---
+++ task 1/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type        = string
+}
+
+variable "ami_id" {
+  type        = string
+}
+
+variable "instance_type" {
+  type        = string
+}
+
+variable "subnet_id" {
+  type        = string
+}
+
+variable "ebs_volume_size" {
+  type        = number
+}
