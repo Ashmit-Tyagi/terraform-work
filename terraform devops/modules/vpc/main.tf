38b93b1ca9043e5a808e50b7da2454de8f0d6ec9
Ashmit Tyagi <151730397+Ashmit-Tyagi@users.noreply.github.com>
2025-05-25 20:44:49 +0530

    Delete terraform devops directory

--- terraform devops/modules/vpc/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_vpc" "vpc1" {
-  cidr_block = var.vpc_cidr_block
-  tags = var.vpc_tags
-}
--- terraform devops/modules/vpc/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "vpc_cidr_block" {
-  type        = string
-}
-
-variable "vpc_tags" {
-    type = map(string)
-}
